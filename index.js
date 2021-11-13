37bf3ca738d35196fb604135b05d7624d5052df4
kngurley <kngurley25@gmail.com>
2021-11-13 17:02:50 -0600

    updated index for inquirer prompt

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+// const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const inquirerQuestions = require("./utils/questions");
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(inquirerQuestions)
+        .then(answers => {
+            console.log(answers);
+        })
+}
 
 // Function call to initialize app
 init();
